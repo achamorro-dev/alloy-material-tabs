2a0645c6fc889d0848998d9f2f6f3fc183e3c32f
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2015-02-24 23:19:59 +0100

    First tabs version

---
+++ app/controllers/tabs.js
@@ -0,0 +1,14 @@
+var tabActive=$.tab1;
+
+function clickTabItem(e){
+	// Movemos a la vista de ScrollableView
+	$.tabs_views.scrollToView(e.source.idx-1);
+};
+
+function scrollTabsViews(e){
+	var viewIdx = e.currentPage + 1;
+	var tabItem = $.getView('tab'+viewIdx);
+	$.removeClass(tabActive,"tab-item-active");
+	$.addClass(tabItem,"tab-item-active");
+	tabActive=tabItem;
+};
